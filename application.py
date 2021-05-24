ad6f09be379136dc603c1e1919f75e9773907493
Obinna <obinna.okafor01@gmail.com>
2021-05-24 14:18:09 +0100

    update app script to use utils module

--- application.py
+++ application.py
@@ -1,39 +1,33 @@
 import os
-import json
 import time
 from uuid import uuid4
 from flask import Flask
+
+from utils import *
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
 data_file = "data.json"
 
+
 @app.route("/")
 def index():
     filepath = os.path.join(os.getcwd(), data_file)
 
     data = read_data_file(filepath)
-
     now = time.strftime("%Y-%m-%d %H:%M:%S")
     new_uuid = uuid4().hex
 
     updated_data = {now: new_uuid, **data}
-    print(updated_data)
-    
+
+    write_data_file(filepath, updated_data)
+        
     return updated_data
 
 
-def read_data_file(filepath):
-    """
-    Reads the content of the file handler passed in,
-    deserializes it into a python object and returns the object
-    """
-    with open(filepath, "r") as fh:
-        content = fh.read()
-        if len(content) > 0:
-            json_content = json.loads(content)
-        else:
-            json_content = {}
+@app.errorhandler(404)
+def page_not_found(error):
+    return {"error": "Not Found"}, 404
 
-    return json_content
+    
