c7789ed3f20aaa903aae2caed45bd307534fcfec
Obinna <obinna.okafor01@gmail.com>
2021-05-24 11:34:43 +0100

    read data file

--- application.py
+++ application.py
@@ -1,3 +1,5 @@
+import os
+import json
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,4 +8,21 @@ data_file = "data.json"
 
 @app.route("/")
 def index():
-    return "Welcome to Cowrywise"
+    filepath = os.path.join(os.getcwd(), data_file)
+
+    file_content = read_data_file(filepath)
+
+
+def read_data_file(filepath):
+    """
+    Reads the content of the file handler passed in,
+    deserializes it into a python object and returns the object
+    """
+    with open(filepath, "r") as fh:
+        content = fh.read()
+        if len(content) > 0:
+            json_content = json.loads(content)
+        else:
+            json_content = {}
+
+    return json_content
